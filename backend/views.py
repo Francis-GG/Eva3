266f584fbfe5c5c7653d48b681dc9fc861cc9c63
Francis-GG <frai.gonzalez@duocuc.cl>
2022-06-16 10:24:51 -0400

    (add) login

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Usuario
+from django.http import HttpResponse
+from urllib import response
+
 
 # Create your views here.
 
 def login(request):
     return render(request, 'login.html')
+
+def validarUsuario (request):
+    v_email=request.POST.get("email")
+    v_password=request.POST.get("password")
+
+    try:    
+        usu=Usuario.objects.get(email=v_email, password=v_password)
+
+        if usu:
+            request.session['usuario'] = v_email
+            return redirect('/productos/')
+    except:
+        return redirect('/login/')
+
+def indexBack(request):
+    if 'usuario' not in request.session:
+        return redirect('/login/')
+    return redirect('/productos/')
